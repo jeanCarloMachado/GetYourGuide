935710c8f8a62254e135074f781520f6856e7a1d
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2017-11-22 22:41:34 -0200

    filter by date tests

--- src/GetYourGuideTest.php
+++ src/GetYourGuideTest.php
@@ -30,4 +30,58 @@ class GetYourGuideTest extends \PHPUnit\Framework\TestCase
         $result = $service->getList('2017-01-01T19:30', '2020-12-31T22:30', 4);
         $this->assertCount(1, $result);
     }
+
+    public function testGetList_shouldFilterByStartDate()
+    {
+        $mock = $this->prophesize(GetYourGuideGateway::class);
+        $mock->fetchRemoteList()->willReturn([
+            [
+                'product_id'=>666,
+                'activity_start_datetime'=>'1990-11-01T19:30',
+                'activity_duration_in_minutes'=>'60',
+                'places_available'=>'3',
+            ],
+        ]);
+        $service = new GetYourGuide($mock->reveal());
+        $result = $service->getList('2017-01-01T19:30', '2020-12-31T22:30', 4);
+        $this->assertCount(0, $result);
+    }
+
+    public function testGetList_shouldFilterByEndDate()
+    {
+        $mock = $this->prophesize(GetYourGuideGateway::class);
+        $mock->fetchRemoteList()->willReturn([
+            [
+                'product_id'=>666,
+                'activity_start_datetime'=>'2999-11-01T19:30',
+                'activity_duration_in_minutes'=>'60',
+                'places_available'=>'3',
+            ],
+        ]);
+        $service = new GetYourGuide($mock->reveal());
+        $result = $service->getList('2017-01-01T19:30', '2020-12-31T22:30', 4);
+        $this->assertCount(0, $result);
+    }
+
+    public function testGetList_shouldOrderByProductId()
+    {
+        $mock = $this->prophesize(GetYourGuideGateway::class);
+        $mock->fetchRemoteList()->willReturn([
+            [
+                'product_id'=>666,
+                'activity_start_datetime'=>'2017-11-01T19:30',
+                'activity_duration_in_minutes'=>'60',
+                'places_available'=>'3',
+            ],
+            [
+                'product_id'=>555,
+                'activity_start_datetime'=>'2017-11-01T19:30',
+                'activity_duration_in_minutes'=>'60',
+                'places_available'=>'123',
+            ]
+        ]);
+        $service = new GetYourGuide($mock->reveal());
+        $result = $service->getList('2017-01-01T19:30', '2020-12-31T22:30', 4);
+        $this->assertEquals(555, $result[0]['product_id']);
+    }
 }
